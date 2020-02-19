186e4b018a827381e3b8341987f45a157091aa63
Jason Marlow <jasonmarlow@gmail.com>
2020-02-18 20:14:27 -0700

    added another example

--- protobufs/main.go
+++ protobufs/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	simple2pb "github.com/jmarlow4/learning-go/protobufs/src/simple"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
-	fmt.Println("Hello, Go!")
+	newMessage := doSimple()
+	newMessage.Name = "Changing the name..."
+	fmt.Println(newMessage.GetName())
+}
+
+func doSimple() *simple2pb.SimpleMessage {
+	sm := simple2pb.SimpleMessage{
+		Id:         16,
+		IsSimple:   true,
+		Name:       "Just a simple message",
+		SampleList: []int32{8, 16, 32, 64},
+	}
+	return &sm
 }
